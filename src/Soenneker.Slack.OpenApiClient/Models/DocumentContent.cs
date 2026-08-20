b155cc4416e8d41c7f52d0a0808f61453137fd81
Jake Soenneker <jake@soenneker.com>
2026-08-20 17:30:43 +0000

    Automated update

--- src/Soenneker.Slack.OpenApiClient/Models/DocumentContent.cs
+++ src/Soenneker.Slack.OpenApiClient/Models/DocumentContent.cs
@@ -8,7 +8,7 @@ using System;
 namespace Soenneker.Slack.OpenApiClient.Models
 {
     /// <summary>
-    /// Structure describing the type and contents.
+    /// Structure describing the type and contents of the Canvas being created.
     /// </summary>
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     public partial class DocumentContent : IAdditionalDataHolder, IParsable
